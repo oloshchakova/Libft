26710795f3f5849f294ea8e819dfa6d693237443
oloshchakova <loshchakova.olga@gmail.com>
2023-02-08 18:53:28 +0100

    add itoa.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -9,8 +9,66 @@
 /*   Updated: 2023/01/20 12:01:26 by oloshcha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
 
-char *ft_itoa(int n)
+int	cnt_len(int n)
 {
+	int	len_num;
 
+	len_num = 0;
+	if (n <= 0 && n != -2147483648)
+	{
+		len_num += 1;
+		n = -n;
+	}
+	while (n != 0)
+	{
+		len_num++;
+		n = n / 10;
+	}
+	return (len_num);
+}
+
+void	*fill_str(char *str, int len_num, int n)
+{
+	int	i;
+
+	i = len_num - 1;
+	while (n >= 10)
+	{
+		str[i] = n % 10 + '0';
+		n = n / 10;
+		i--;
+	}
+	str[i] = n % 10 + '0';
+	str[len_num] = '\0';
+	return (str);
+}
+
+char	*ft_itoa(int n)
+{
+	int		len_num;
+	char	*str;
+
+	if (n == -2147483648)
+		len_num = 11;
+	else
+		len_num = cnt_len(n);
+	str = (char *)malloc(sizeof(char) * len_num + 1);
+	if (!str)
+		return (0);
+	if (n == -2147483648)
+	{
+		fill_str(str, len_num, -(n + 1));
+		str[0] = '-';
+		str[len_num - 1] = '8';
+	}
+	else if (n < 0)
+	{
+		fill_str(str, len_num, -n);
+		str[0] = '-';
+	}
+	else
+		fill_str(str, len_num, n);
+	return (str);
 }
